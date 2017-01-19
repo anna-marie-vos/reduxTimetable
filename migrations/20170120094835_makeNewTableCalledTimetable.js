ecc585edcaf5c7579d38e5b066583021e000f40f
JanieT <jtito@hotmail.com>
2017-01-20 10:41:35 +1300

    basic db setup done

---
+++ migrations/20170120094835_makeNewTableCalledTimetable.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTableIfNotExists('timetable', function (table) {
+    table.string('day');
+    table.string('period');
+    table.string('subject');
+    table.string('teacher');
+    table.string('classroom');
+    table.string('details');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('timetable');
+};
